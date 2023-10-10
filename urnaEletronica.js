bc8b506f809eff69a924fe617612cdd28a2c49a8
FelipeWilquem <142447639+FelipeWilquem@users.noreply.github.com>
2023-10-10 19:51:11 +0000

    Feat Criando urna

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,53 +1,73 @@
-//estrutura :switch-case(aula hj)                   ------estudar aula de ontem (if) (else) (else -if)
-const opcao = 1;
+// //estrutura :switch-case(aula hj)                   ------estudar aula de ontem (if) (else) (else -if)
+// const opcao = 1;
 
-switch(opcao) {
+// switch(opcao) {
 
-    case:1
-         console.log('opção1');
-          break; //pare
-    case:2
-         console.log('opção2');
-         break; //pare
-    default: //caso contrario
-    case:3
-         console.log('nenhuma dsa opções');
+//     case:1
+//          console.log('opção1');
+//           break; //pare
+//     case:2
+//          console.log('opção2');
+//          break; //pare
+//     default: //caso contrario
+//     case:3
+//          console.log('nenhuma dsa opções');
    
+// }
+
+// // equivilante a estrutura switch -case
+// if (opcao === 1) {
+//     console.log('opção é 1')
+// }else if (opcao === 2) {
+//     console.log ('opção é 2')
+// } else {
+//     console.log('nenhuma das opcões')
+// }
+
+
+// //estrutura de repetição: do-wilhe (faça enquanto)
+// //do = faça
+
+
+
+// do {
+//     console.log('Instrução 1');
+//     console.log('Instrução 2');
+//     console.log('Instrução 3');
+//     console.log('Instrução 4');
+//     console.log('Instrução 5');
+//     console.log('Instrução 6');
+//  }while (opcao !== 0);
+
+//  const opcao = 0;
+
+//  //estrutura de repetição: while (enquanto)
+
+//  while ('opcao !== 0') {
+//     console.log('Instrução 1');
+//     console.log('Instrução 2');
+//     console.log('Instrução 3');
+//     console.log('Instrução 4');
+//     console.log('Instrução 5');
+//     console.log('Instrução 6');
+//  }
+
+
+
+function urnaEletronica() {
+    console.log('Iniciando o programa')
 }
 
-// equivilante a estrutura switch -case
-if (opcao === 1) {
-    console.log('opção é 1')
-}else if (opcao === 2) {
-    console.log ('opção é 2')
-} else {
-    console.log('nenhuma das opcões')
-}
 
-
-//estrutura de repetição: do-wilhe (faça enquanto)
-//do = faça
-
-
+let totalVotosCandidatos1 = 0;
+let totalVotosCandidatos2 = 0;
+let totalVotosCandidatos3 = 0;
+let totalVotosBrancos = 0;
+let totalVotosNulo = 0;
 
 do {
-    console.log('Instrução 1');
-    console.log('Instrução 2');
-    console.log('Instrução 3');
-    console.log('Instrução 4');
-    console.log('Instrução 5');
-    console.log('Instrução 6');
- }while (opcao !== 0);
+    
+}
 
- const opcao = 0;
 
- //estrutura de repetição: while (enquanto)
 
- while ('opcao !== 0') {
-    console.log('Instrução 1');
-    console.log('Instrução 2');
-    console.log('Instrução 3');
-    console.log('Instrução 4');
-    console.log('Instrução 5');
-    console.log('Instrução 6');
- }
