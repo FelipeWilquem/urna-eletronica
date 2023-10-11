6f9bf102d7d9b5328d840abade342e21a9f658aa
felipe <felipe@senai>
2023-10-11 16:27:44 -0300

    feat base do codigo

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -12,7 +12,7 @@
 //     default: //caso contrario
 //     case:3
 //          console.log('nenhuma dsa opções');
-   
+
 // }
 
 // // equivilante a estrutura switch -case
@@ -56,18 +56,59 @@
 
 function urnaEletronica() {
     console.log('Iniciando o programa')
+
+
+
+    let totalVotosCandidatos1 = 0;
+    let totalVotosCandidatos2 = 0;
+    let totalVotosCandidatos3 = 0;
+    let totalVotosBrancos = 0;
+    let totalVotosNulo = 0;
+
+
+
+    let voto;
+
+    do {
+        console.log('1=candidato1');
+        console.log('2=candidato2');
+        console.log('3=candidato3');
+        console.log('5=voto em branco');
+        console.log('8=voto nulo');
+        console.log('0=encerrar');
+
+        voto = parseInt(prompt('digite seu voto:'));
+
+        if (voto === 1){
+            totalVotosCandidatos1++;
+            console.log('o candidato 1 recebeu um voto');
+        }else if (voto === 2){
+            totalVotosCandidatos2++;
+            console.log('o candidato 2 recebeu um voto');
+        }else if (voto === 3){
+            totalVotosCandidatos3++;
+            console.log('o candidato 3 recebeu um voto');
+        }else if (voto === 5){
+            totalVotosBrancos++;
+            console.log('o candidato recebeu um voto');
+        }else if(voto === 8){
+            totalVotosNulo++;
+            console.log('o candidato recebeu um voto');
+        }else if(voto === 0){
+            console.log('Encerrar a votação');
+        }else{
+            return;
+        }
+  
+    }while (voto!== 0); // enquanto o voto for extritamente diferente de zero, repita a ação
+
+    const  
+
+
+
+
+
+
+
+
 }
-
-
-let totalVotosCandidatos1 = 0;
-let totalVotosCandidatos2 = 0;
-let totalVotosCandidatos3 = 0;
-let totalVotosBrancos = 0;
-let totalVotosNulo = 0;
-
-do {
-    
-}
-
-
-
